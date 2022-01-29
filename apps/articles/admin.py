6a5704f51b97789b7530a1aeb072badd90d8ec7b
nsorokopud <96976346+nsorokopud@users.noreply.github.com>
2022-01-30 00:28:09 +0200

    Create Article model

--- apps/articles/admin.py
+++ apps/articles/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import ArticleCategory
+from .models import Article, ArticleCategory
 
 
+admin.site.register(Article)
 admin.site.register(ArticleCategory)
--- apps/articles/models.py
+++ apps/articles/models.py
@@ -1,4 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+from ckeditor_uploader.fields import RichTextUploadingField
+from taggit.managers import TaggableManager
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=256)
+    slug = models.SlugField(max_length=256, unique=True)
+    category = models.ForeignKey("ArticleCategory", null=True, on_delete=models.SET_NULL)
+    tags = TaggableManager(blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    preview_text = models.CharField(max_length=512)
+    preview_image = models.ImageField(upload_to="articles/preview_images/", null=True, blank=True)
+    content = RichTextUploadingField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name_plural = "Articles"
+
+    def __str__(self):
+        return f"{self.title} - {self.author} - [{self.created_at}]"
 
 
 class ArticleCategory(models.Model):
