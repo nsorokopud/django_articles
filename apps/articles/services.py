d29335c612a68dfa6fc7cbe673bc45fe50df50ea
nsorokopud <96976346+nsorokopud@users.noreply.github.com>
2022-01-30 22:57:10 +0200

    Create a service function to get all categories of articles

--- apps/articles/services.py
+++ apps/articles/services.py
@@ -1,7 +1,11 @@
 from typing import Iterable
 
-from .models import Article
+from .models import Article, ArticleCategory
 
 
 def find_published_articles() -> Iterable[Article]:
     return Article.objects.filter(is_published=True)
+
+
+def get_all_categories() -> Iterable[ArticleCategory]:
+    return ArticleCategory.objects.all()
