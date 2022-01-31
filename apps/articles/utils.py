56d9ae457e323800eb69d5a0f64aca7e9a0286e3
nsorokopud <96976346+nsorokopud@users.noreply.github.com>
2022-01-31 17:24:16 +0200

    Modify CategoriesMixin to accept kwargs in get_context_data mathod

--- apps/articles/utils.py
+++ apps/articles/utils.py
@@ -2,7 +2,7 @@ from .services import get_all_categories
 
 
 class CategoriesMixin:
-    def get_context_data(self):
-        context = super().get_context_data()
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
         context["categories"] = get_all_categories()
         return context
