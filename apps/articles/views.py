b8de819ff21dd028e12f911ce321d119974f0cae
nsorokopud <96976346+nsorokopud@users.noreply.github.com>
2022-01-31 15:24:46 +0200

    Create a view and a url route for the home page

--- apps/articles/views.py
+++ apps/articles/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import Article
+from .services import find_published_articles
+from .utils import CategoriesMixin
+
+
+class HomePageView(CategoriesMixin, ListView):
+    model = Article
+    context_object_name = "articles"
+    paginate_by = 5
+    template_name = "articles/home_page.html"
+
+    def get_queryset(self):
+        return find_published_articles()
