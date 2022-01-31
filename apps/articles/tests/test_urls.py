bca0a2c1c0e00b2e183ea1fc7fc2efa15c6bd183
nsorokopud <96976346+nsorokopud@users.noreply.github.com>
2022-01-31 18:08:04 +0200

    Add tests for the article details page

--- apps/articles/tests/test_urls.py
+++ apps/articles/tests/test_urls.py
@@ -1,10 +1,32 @@
 from django.test import TestCase
 from django.urls import reverse, resolve
+from django.contrib.auth.models import User
 
-from articles.views import HomePageView
+from articles.views import HomePageView, ArticleDetailView
+from articles.models import Article, ArticleCategory
 
 
 class TestViews(TestCase):
+    def setUp(self):
+        self.test_user = User(username="test_user", email="test@test.com")
+        self.test_user.set_password("12345")
+        self.test_user.save()
+
+        self.test_category = ArticleCategory.objects.create(title="cat1", slug="cat1")
+        self.test_article = Article.objects.create(
+            title="a1",
+            slug="a1",
+            category=self.test_category,
+            author=self.test_user,
+            preview_text="text1",
+            content="content1",
+            is_published=True,
+        )
+
     def test_homepage_url_is_resolved(self):
         url = reverse("home")
         self.assertEquals(resolve(url).func.view_class, HomePageView)
+
+    def test_article_details_page_url_is_resolved(self):
+        url = reverse("article-details", args=[self.test_article.slug])
+        self.assertEquals(resolve(url).func.view_class, ArticleDetailView)
--- apps/articles/tests/test_views.py
+++ apps/articles/tests/test_views.py
@@ -1,13 +1,37 @@
 from django.test import TestCase, Client
 from django.urls import reverse
+from django.contrib.auth.models import User
+
+from articles.models import Article, ArticleCategory
 
 
 class TestViews(TestCase):
     def setUp(self):
         self.client = Client()
 
+        self.test_user = User(username="test_user", email="test@test.com")
+        self.test_user.set_password("12345")
+        self.test_user.save()
+
+        self.test_category = ArticleCategory.objects.create(title="cat1", slug="cat1")
+        self.test_article = Article.objects.create(
+            title="a1",
+            slug="a1",
+            category=self.test_category,
+            author=self.test_user,
+            preview_text="text1",
+            content="content1",
+            is_published=True,
+        )
+
     def test_homepage_view(self):
         response = self.client.get(reverse("home"))
 
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed("articles/home_page.html")
+
+    def test_article_details_page_view(self):
+        response = self.client.get(reverse("article-details", args=[self.test_article.slug]))
+
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed("articles/article.html")
