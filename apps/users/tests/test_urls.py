14379977f85b3447f9ceae753637d9d5e3661446
nsorokopud <96976346+nsorokopud@users.noreply.github.com>
2022-02-01 18:53:37 +0200

    Add tests for logging in

--- apps/users/tests/test_urls.py
+++ apps/users/tests/test_urls.py
@@ -1,10 +1,14 @@
 from django.test import TestCase
 from django.urls import reverse, resolve
 
-from users.views import UserRegistrationView
+from users.views import UserRegistrationView, UserLoginView
 
 
 class TestViews(TestCase):
     def test_user_registration_url_is_resolved(self):
         url = reverse("registration")
         self.assertEquals(resolve(url).func.view_class, UserRegistrationView)
+
+    def test_user_login_url_is_resolved(self):
+        url = reverse("login")
+        self.assertEquals(resolve(url).func.view_class, UserLoginView)
--- apps/users/tests/test_views.py
+++ apps/users/tests/test_views.py
@@ -7,6 +7,10 @@ class TestViews(TestCase):
     def setUp(self):
         self.client = Client()
 
+        self.test_user = User(username="test_user", email="test@test.com")
+        self.test_user.set_password("12345")
+        self.test_user.save()
+
     def test_user_registration_view_get(self):
         response = self.client.get(reverse("registration"))
         self.assertEquals(response.status_code, 200)
@@ -24,3 +28,19 @@ class TestViews(TestCase):
         self.assertRedirects(response, reverse("login"), status_code=302, target_status_code=200)
         user = User.objects.order_by("id").last()
         self.assertEquals(user.username, user_data["username"])
+
+    def test_user_login_view_get(self):
+        response = self.client.get(reverse("login"))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed("users/login.html")
+
+    def test_user_login_view_post(self):
+        login_data = {"username": "test_user", "password": "12345"}
+
+        response = self.client.get("login")
+        self.assertEquals(response.wsgi_request.user.is_authenticated, False)
+
+        response = self.client.post(reverse("login"), login_data)
+        self.assertRedirects(response, reverse("home"), status_code=302, target_status_code=200)
+        self.assertEquals(response.wsgi_request.user.is_authenticated, True)
+        self.assertEquals(response.wsgi_request.user, self.test_user)
