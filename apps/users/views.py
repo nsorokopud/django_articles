2be7641ac0f003e2cac5c15a8dafe5848a6883c4
nsorokopud <96976346+nsorokopud@users.noreply.github.com>
2022-02-01 18:25:46 +0200

    Create a view and a url route for user registration

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+
+class UserRegistrationView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    template_name = "users/registration.html"
+    success_url = reverse_lazy("login")
