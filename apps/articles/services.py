10a2f795fd03f7cf314c718603b83d35a1029588
nsorokopud <96976346+nsorokopud@users.noreply.github.com>
2022-02-02 14:36:22 +0200

    Create a service function for creating articles

--- apps/articles/services.py
+++ apps/articles/services.py
@@ -1,4 +1,7 @@
-from typing import Iterable
+from typing import Iterable, List, Optional
+
+from django.contrib.auth.models import User
+from django.template.defaultfilters import slugify
 
 from .models import Article, ArticleCategory
 
@@ -9,3 +12,29 @@ def find_published_articles() -> Iterable[Article]:
 
 def get_all_categories() -> Iterable[ArticleCategory]:
     return ArticleCategory.objects.all()
+
+
+def create_article(
+    title: str,
+    category: ArticleCategory,
+    author: User,
+    preview_text: str,
+    content: str,
+    tags: Optional[List] = None,
+    preview_image: Optional[str] = None,
+):
+    article = Article.objects.create(
+        title=title,
+        category=category,
+        author=author,
+        preview_text=preview_text,
+        preview_image=preview_image,
+        content=content,
+        is_published=True,
+    )
+    article.slug = slugify(title)
+    if not tags:
+        tags = []
+    article.tags.add(*tags)
+    article.save()
+    return article
