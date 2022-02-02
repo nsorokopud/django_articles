7136d98368bc51cd8483c42d42623f86963d8cc0
nsorokopud <96976346+nsorokopud@users.noreply.github.com>
2022-02-02 14:47:06 +0200

    Create a view and a url route for the article creation page

--- apps/articles/urls.py
+++ apps/articles/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     path("", views.HomePageView.as_view(), name="home"),
+    path("articles/create", views.ArticleCreateView.as_view(), name="article-create"),
     path("articles/<slug:article_slug>", views.ArticleDetailView.as_view(), name="article-details")
 ]
--- apps/articles/views.py
+++ apps/articles/views.py
@@ -1,6 +1,9 @@
-from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Article
+from .forms import ArticleCreateForm
 from .services import find_published_articles
 from .utils import CategoriesMixin
 
@@ -20,3 +23,15 @@ class ArticleDetailView(CategoriesMixin, DetailView):
     slug_url_kwarg = "article_slug"
     context_object_name = "article"
     template_name = "articles/article.html"
+
+
+class ArticleCreateView(LoginRequiredMixin, CategoriesMixin, CreateView):
+    model = Article
+    form_class = ArticleCreateForm
+    template_name = "articles/article_create.html"
+    login_url = reverse_lazy("login")
+
+    def get_form_kwargs(self):
+        kwargs = super(ArticleCreateView, self).get_form_kwargs()
+        kwargs["request"] = self.request
+        return kwargs
