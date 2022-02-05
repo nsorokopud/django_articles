c3a65fd418439520b9d2dfc30d58a8c66cacf8b8
nsorokopud <96976346+nsorokopud@users.noreply.github.com>
2022-02-05 18:14:11 +0200

    Register ArticleComment model in admin panel

--- apps/articles/admin.py
+++ apps/articles/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Article, ArticleCategory
+from .models import Article, ArticleCategory, ArticleComment
 
 
 admin.site.register(Article)
 admin.site.register(ArticleCategory)
+admin.site.register(ArticleComment)
