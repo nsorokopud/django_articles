205eb3ff757b40c060c25a33efb916b580f743a6
nsorokopud <96976346+nsorokopud@users.noreply.github.com>
2022-02-06 17:12:22 +0200

    Add a test to check if js is loading

--- tests/test_static.py
+++ tests/test_static.py
@@ -4,5 +4,9 @@ from django.contrib.staticfiles import finders
 
 class TestStatic(TestCase):
     def test_css_is_loading(self):
-        css_path = finders.find('css/style.css')
+        css_path = finders.find("css/style.css")
         self.assertIsNotNone(css_path)
+
+    def test_js_is_loading(self):
+        js_path = finders.find("js/like.js")
+        self.assertIsNotNone(js_path)
