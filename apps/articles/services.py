825880b58b76a90961f3480694cf9a8750359665
nsorokopud <96976346+nsorokopud@users.noreply.github.com>
2022-02-08 13:02:32 +0200

    Create a service function for toggling comment likes

--- apps/articles/services.py
+++ apps/articles/services.py
@@ -6,7 +6,7 @@ from django.db.models.query import QuerySet
 from django.contrib.auth.models import User
 from django.template.defaultfilters import slugify
 
-from .models import Article, ArticleCategory
+from .models import Article, ArticleCategory, ArticleComment
 
 
 logger = logging.getLogger("default_logger")
@@ -21,12 +21,16 @@ def find_articles_of_category(category_slug: str) -> QuerySet[Article]:
 
 
 def find_articles_by_query(q: str) -> QuerySet[Article]:
-    return find_published_articles().filter(
-        Q(title__icontains=q)
-        | Q(category__title__icontains=q)
-        | Q(tags__name__icontains=q)
-        | Q(content__icontains=q)
-    ).distinct()
+    return (
+        find_published_articles()
+        .filter(
+            Q(title__icontains=q)
+            | Q(category__title__icontains=q)
+            | Q(tags__name__icontains=q)
+            | Q(content__icontains=q)
+        )
+        .distinct()
+    )
 
 
 def get_all_categories() -> Iterable[ArticleCategory]:
@@ -82,4 +86,19 @@ def toggle_article_like(article_slug: str, user_id: int) -> Optional[int]:
         return article.get_likes_count()
     except Article.DoesNotExist:
         logger.error(f"Tried to get a non-existent article with slug={article_slug}")
+        return None
+
+
+def toggle_comment_like(comment_id: int, user_id: int) -> Optional[int]:
+    try:
+        comment = ArticleComment.objects.get(id=comment_id)
+        try:
+            user = comment.users_that_liked.get(id=user_id)
+            comment.users_that_liked.remove(user)
+        except User.DoesNotExist:
+            user = User.objects.get(id=user_id)
+            comment.users_that_liked.add(user)
+        return comment.get_likes_count()
+    except ArticleComment.DoesNotExist:
+        logger.error(f"Tried to get a non-existent comment with id={comment_id}")
         return None
