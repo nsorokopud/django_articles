f16e6d2ba5b0b21ce39e208fca116de00df1b95c
nsorokopud <96976346+nsorokopud@users.noreply.github.com>
2022-02-09 12:41:52 +0200

    Create a view and a url route for the user profile page

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("register/", views.UserRegistrationView.as_view(), name="registration"),
     path("login/", views.UserLoginView.as_view(), name="login"),
     path("logout/", views.UserLogoutView.as_view(), name="logout"),
+    path("user/profile/", views.UserProfileView.as_view(), name="user-profile"),
 ]
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,8 +1,14 @@
-from django.urls import reverse_lazy
+from django.shortcuts import redirect, render
+from django.urls import reverse, reverse_lazy
+from django.views import View
 from django.views.generic import CreateView
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from .forms import UserUpdateForm, ProfileUpdateForm
+from .services import delete_profile_image
 
 
 class UserRegistrationView(CreateView):
@@ -18,3 +24,29 @@ class UserLoginView(LoginView):
 
 class UserLogoutView(LogoutView):
     template_name = "users/logout.html"
+
+
+class UserProfileView(LoginRequiredMixin, View):
+    login_url = reverse_lazy("login")
+
+    def get(self, request):
+        user_form = UserUpdateForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user.profile)
+
+        context = {"user_form": user_form, "profile_form": profile_form}
+        return render(request, "users/profile.html", context)
+
+    def post(self, request):
+        old_profile_image = request.user.profile.image.path
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileUpdateForm(
+            request.POST, request.FILES, instance=request.user.profile
+        )
+
+        if user_form.is_valid() and profile_form.is_valid():
+            if request.user.profile.image.path != old_profile_image:
+                delete_profile_image(old_profile_image)
+
+            user_form.save()
+            profile_form.save()
+            return redirect(reverse("user-profile"))
