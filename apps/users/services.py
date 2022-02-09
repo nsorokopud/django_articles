f2c2138c3441bab65ce462cfb83ab1ed51a22e75
nsorokopud <96976346+nsorokopud@users.noreply.github.com>
2022-02-09 14:58:11 +0200

    Create a service for creating user profiles

--- apps/users/services.py
+++ apps/users/services.py
@@ -1,6 +1,14 @@
 import os
 
 from django.conf import settings
+from django.contrib.auth.models import User
+
+from .models import Profile
+
+
+def create_user_profile(user: User) -> Profile:
+    profile = Profile.objects.create(user=user)
+    return profile
 
 
 def delete_profile_image(image_name: str) -> None:
