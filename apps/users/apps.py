6d5151fada14d9dc219c692c3b5733bcf9943ac3
nsorokopud <96976346+nsorokopud@users.noreply.github.com>
2022-02-09 15:00:09 +0200

    Create a signal for creating user profiles

--- apps/users/apps.py
+++ apps/users/apps.py
@@ -1,6 +1,13 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_save
 
 
 class UsersConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'users'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "users"
+
+    def ready(self):
+        from django.contrib.auth.models import User
+        from .signals import create_profile
+
+        post_save.connect(create_profile, sender=User)
---
+++ apps/users/signals.py
@@ -0,0 +1,6 @@
+from .services import create_user_profile
+
+
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        create_user_profile(user=instance)
