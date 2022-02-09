c545d56e8fc761c27c0d48ecebd506a3bc957b89
nsorokopud <96976346+nsorokopud@users.noreply.github.com>
2022-02-09 12:36:10 +0200

    Create a form for updating user profile

--- apps/users/forms.py
+++ apps/users/forms.py
@@ -8,3 +8,9 @@ class UserUpdateForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ["username"]
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ["image"]
