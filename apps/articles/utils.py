18d2ca3e6b61040f67be6601f592fff2a446f74f
nsorokopud <96976346+nsorokopud@users.noreply.github.com>
2022-02-17 13:54:25 +0200

    Create ArticlesListMixin

--- apps/articles/utils.py
+++ apps/articles/utils.py
@@ -1,13 +1,8 @@
 from django.http import Http404
 
+from articles.constants import ARTICLES_PER_PAGE_COUNT
+from articles.models import Article
 from articles.services import get_all_categories
-
-
-class CategoriesMixin:
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["categories"] = get_all_categories()
-        return context
 
 
 class AllowOnlyAuthorMixin:
@@ -16,3 +11,17 @@ class AllowOnlyAuthorMixin:
             raise Http404()
         else:
             return super().dispatch(*args, **kwargs)
+
+
+class ArticlesListMixin:
+    model = Article
+    context_object_name = "articles"
+    paginate_by = ARTICLES_PER_PAGE_COUNT
+    template_name = "articles/home_page.html"
+
+
+class CategoriesMixin:
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["categories"] = get_all_categories()
+        return context
