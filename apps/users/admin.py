9f1e01e0e3f451c3f413f603d3cd62bbaf9b183c
nsorokopud <96976346+nsorokopud@users.noreply.github.com>
2022-02-18 16:10:06 +0200

    Create admin models for users app

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from users.models import Profile
 
 
-admin.site.register(Profile)
+@admin.register(Profile)
+class ArticleCommentAdmin(admin.ModelAdmin):
+    list_display = ("user", "get_profile_image")
+    search_fields = ("user__username",)
+    readonly_fields = ("user",)
+
+    def get_profile_image(self, profile):
+        return mark_safe(f"<img src={profile.image.url} width='35' height='35'>")
