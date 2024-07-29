d6d05b3623b5dac6fa9881af06626106ab25e126
nsorokopud <96976346+nsorokopud@users.noreply.github.com>
2024-07-29 19:59:03 +0100

    Create model for notifications

--- apps/notifications/admin.py
+++ apps/notifications/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Notification
+
+
+class NotificationAdmin(admin.ModelAdmin):
+    readonly_fields = ("created_at",)
+
+
+admin.site.register(Notification, NotificationAdmin)
--- apps/notifications/models.py
+++ apps/notifications/models.py
@@ -1,3 +1,33 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Notification(models.Model):
+    class Type(models.TextChoices):
+        NEW_ARTICLE = "new article"
+        NEW_COMMENT = "new comment"
+
+    class Status(models.TextChoices):
+        UNREAD = "unread"
+        READ = "read"
+
+    type = models.CharField(max_length=255, choices=Type)
+    title = models.CharField(max_length=255, null=True, blank=True)
+    message = models.CharField(max_length=500, null=True, blank=True)
+    link = models.URLField(max_length=500, null=True, blank=True)
+    sender = models.ForeignKey(
+        User, null=True, blank=True, related_name="sent_notifications", on_delete=models.SET_NULL
+    )
+    recepient = models.ForeignKey(
+        User, blank=True, related_name="received_notifications", on_delete=models.CASCADE
+    )
+    status = models.CharField(
+        max_length=255, null=True, blank=True, choices=Status, default=Status.UNREAD
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        created_at = self.created_at.strftime("%H:%M:%S %d-%m-%Y")
+        sender = self.sender.username
+        recepient = self.recepient.username
+        return f"{created_at} [{self.type}] from {sender} to {recepient}: {self.link}"
