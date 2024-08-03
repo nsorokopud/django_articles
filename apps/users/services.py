da3a8bac3cb47d0c770d618eb8c91a1d815cdb2c
nsorokopud <96976346+nsorokopud@users.noreply.github.com>
2024-08-03 17:48:56 +0100

    Create a function for selecting a user object by id

--- apps/users/services.py
+++ apps/users/services.py
@@ -6,3 +6,7 @@ from users.models import Profile
 def create_user_profile(user: User) -> Profile:
     profile = Profile.objects.create(user=user)
     return profile
+
+
+def get_user_by_id(user_id: int) -> User:
+    return User.objects.get(id=user_id)
--- apps/users/tests/test_services.py
+++ apps/users/tests/test_services.py
@@ -3,7 +3,7 @@ from django.db.models import signals
 from django.test import TestCase
 
 from users.models import Profile
-from users.services import create_user_profile
+from users.services import create_user_profile, get_user_by_id
 from users.signals import create_profile
 
 
@@ -21,3 +21,19 @@ class TestServices(TestCase):
         profile = create_user_profile(self.test_user)
         self.assertEqual(profile.user, self.test_user)
         self.assertEqual(Profile.objects.filter(user=self.test_user).first(), profile)
+
+    def test_get_user_by_id(self):
+        u1 = get_user_by_id(self.test_user.id)
+        self.assertEqual(u1, self.test_user)
+
+        u1_id = u1.id
+        next_user_id = u1_id + 1
+        with self.assertRaises(User.DoesNotExist):
+            get_user_by_id(next_user_id)
+
+        u2 = User.objects.create(username="user2", email="test2@test.com")
+        next_user = get_user_by_id(next_user_id)
+        self.assertEqual(next_user, u2)
+
+        with self.assertRaises(User.DoesNotExist):
+            get_user_by_id(999)
