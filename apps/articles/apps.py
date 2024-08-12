7d9f0f9fc757ac9e6d23a4d0070290a813eb1c79
nsorokopud <96976346+nsorokopud@users.noreply.github.com>
2024-08-12 20:26:03 +0100

    Replace single quotes with double ones in articles/apps.py

--- apps/articles/apps.py
+++ apps/articles/apps.py
@@ -3,8 +3,8 @@ from django.db.models.signals import post_save
 
 
 class ArticlesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'articles'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "articles"
 
     def ready(self):
         from .models import Article, ArticleComment
