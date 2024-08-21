3ebe6e42fb031371d9f89684d984ad169b12f2e0
nsorokopud <96976346+nsorokopud@users.noreply.github.com>
2024-08-21 03:03:43 -0930

    Add cache settings

--- config/settings.py
+++ config/settings.py
@@ -288,6 +288,19 @@ REDIS_HOST = os.environ["REDIS_HOST"]
 REDIS_PORT = os.environ["REDIS_PORT"]
 
 
+# Cache
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": f"redis://{REDIS_HOST}:{REDIS_PORT}/2",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
+
 # Django Channels
 
 CHANNEL_LAYERS = {
