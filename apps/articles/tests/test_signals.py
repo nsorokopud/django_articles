340c34b2e518219b6fbdbf3efe26fba3d0aa4c78
nsorokopud <96976346+nsorokopud@users.noreply.github.com>
2024-09-12 11:11:57 +0100

    Add a test for the signal handler for deleting media files attached to a deleted article

--- apps/articles/tests/test_signals.py
+++ apps/articles/tests/test_signals.py
@@ -5,7 +5,11 @@ from django.db.models import signals
 from django.test import TransactionTestCase
 
 from ..models import Article, ArticleComment
-from ..signals import send_article_notification, send_comment_notification
+from ..signals import (
+    delete_article_media_files,
+    send_article_notification,
+    send_comment_notification,
+)
 
 
 class TestSignals(TransactionTestCase):
@@ -13,6 +17,23 @@ class TestSignals(TransactionTestCase):
     def tearDownClass(cls):
         signals.post_save.connect(send_article_notification, sender=Article)
         signals.post_save.connect(send_comment_notification, sender=ArticleComment)
+        signals.post_delete.connect(delete_article_media_files, sender=Article)
+
+    def test_delete_article_media_files(self):
+        user = User.objects.create(username="user")
+        a1 = Article.objects.create(title="a1", slug="a1", author=user, preview_text="a1", content="a1")
+
+        with patch("articles.signals.delete_media_files_attached_to_article") as mock:
+            a1.delete()
+            mock.assert_called_once_with(a1)
+
+        a2 = Article.objects.create(title="a2", slug="a2", author=user, preview_text="a2", content="a2")
+
+        signals.post_delete.disconnect(delete_article_media_files, sender=Article)
+
+        with patch("articles.signals.delete_media_files_attached_to_article") as mock:
+            a2.delete()
+            mock.assert_not_called()
 
     def test_send_article_notification(self):
         user = User.objects.create_user(username="user")
