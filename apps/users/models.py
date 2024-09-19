e6aa945952076df7c71820d22e85f1e4d7ef58f6
nsorokopud <96976346+nsorokopud@users.noreply.github.com>
2024-09-19 17:45:10 +0100

    Create custom User model

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,5 +1,9 @@
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
 
 
 class Profile(models.Model):
