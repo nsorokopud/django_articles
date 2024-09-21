0049e93fe683e82897d682f8fb758e02cd7bb950
nsorokopud <96976346+nsorokopud@users.noreply.github.com>
2024-09-21 18:35:27 +0100

    Update the POST test of UserLoginView

--- apps/users/tests/test_views.py
+++ apps/users/tests/test_views.py
@@ -3,6 +3,7 @@ from unittest.mock import patch
 from django.test import Client, TestCase
 from django.urls import reverse
 
+from users.forms import AuthenticationForm
 from users.models import User
 
 
@@ -39,15 +40,41 @@ class TestViews(TestCase):
         self.assertTemplateUsed(response, "users/login.html")
 
     def test_user_login_view_post(self):
-        login_data = {"username": "test_user", "password": "12345"}
+        login_data1 = {"username": "test_user", "password": "12345"}
+        login_data2 = {"username": "test@test.com", "password": "12345"}
+        login_data_invalid = {"username": "invalid", "password": "invalid"}
 
         response = self.client.get("login")
-        self.assertEqual(response.wsgi_request.user.is_authenticated, False)
+        self.assertFalse(response.wsgi_request.user.is_authenticated)
 
-        response = self.client.post(reverse("login"), login_data)
+        with patch("hcaptcha_field.hCaptchaField.validate", return_value=True):
+            response = self.client.post(reverse("login"), login_data1)
         self.assertRedirects(response, reverse("articles"), status_code=302, target_status_code=200)
-        self.assertEqual(response.wsgi_request.user.is_authenticated, True)
+        self.assertTrue(response.wsgi_request.user.is_authenticated)
         self.assertEqual(response.wsgi_request.user, self.test_user)
+
+        self.client.logout()
+
+        with patch("hcaptcha_field.hCaptchaField.validate", return_value=True):
+            response = self.client.post(reverse("login"), login_data2)
+        self.assertRedirects(response, reverse("articles"), status_code=302, target_status_code=200)
+        self.assertTrue(response.wsgi_request.user.is_authenticated)
+        self.assertEqual(response.wsgi_request.user, self.test_user)
+
+        self.client.logout()
+
+        with patch("hcaptcha_field.hCaptchaField.validate", return_value=True):
+            response = self.client.post(reverse("login"), login_data_invalid)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "users/login.html")
+        form = response.context["form"]
+        self.assertTrue(isinstance(form, AuthenticationForm))
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors, {"__all__": [
+            "Please enter a correct username and password. Note that both fields may be case-sensitive."
+        ]})
+        self.assertFalse(response.wsgi_request.user.is_authenticated)
+        self.assertNotEqual(response.wsgi_request.user, self.test_user)
 
     def test_user_logout_view_get(self):
         response = self.client.get(reverse("logout"))
