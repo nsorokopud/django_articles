2447127aa9bdde9e78b37eacf0315f06d0bf1399
nsorokopud <96976346+nsorokopud@users.noreply.github.com>
2024-09-22 14:18:55 +0100

    Create a test for the user account activation view

--- apps/users/tests/test_views.py
+++ apps/users/tests/test_views.py
@@ -2,9 +2,12 @@ from unittest.mock import patch
 
 from django.test import Client, TestCase
 from django.urls import reverse
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
 
 from users.forms import AuthenticationForm
 from users.models import User
+from users.tokens import activation_token_generator
 
 
 class TestViews(TestCase):
@@ -127,3 +130,86 @@ class TestViews(TestCase):
         redirect_url = reverse("author-page", args=(author.username,))
         self.assertRedirects(response, redirect_url, status_code=302, target_status_code=200)
         self.assertTrue(self.test_user not in author.profile.subscribers.all())
+
+    def test_account_activation_view(self):
+        user1 = User.objects.create_user(username="user1", email="user1@test.com", password="a12345", is_active=False)
+        user2 = User.objects.create_user(username="user2", email="user2@test.com", password="b12345", is_active=False)
+        user3 = User.objects.create_user(username="user3", email="user3@test.com", password="c12345", is_active=True)
+
+        self.assertFalse(user1.is_active)
+        self.assertFalse(user2.is_active)
+        self.assertTrue(user3.is_active)
+
+        user1_encoded_id = urlsafe_base64_encode(force_bytes(user1.pk))
+        token1 = activation_token_generator.make_token(user1)
+        token2 = activation_token_generator.make_token(user2)
+
+        # correct case
+        self.client.force_login(user3)
+        response = self.client.get(reverse("account-activate", args=[user1_encoded_id, token1]))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "users/account_activation.html")
+        self.assertTrue(response.context["is_activation_successful"])
+        with self.assertRaises(KeyError):
+            response.context["error_message"]
+        user1.refresh_from_db()
+        user2.refresh_from_db()
+        self.assertTrue(user1.is_active)
+        self.assertFalse(user2.is_active)
+        self.assertFalse(response.context["user"].is_authenticated)
+        self.assertTrue(response.context["user"].is_anonymous)
+
+        # trying to activate already activated account
+        response = self.client.get(reverse("account-activate", args=[user1_encoded_id, token1]))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "users/account_activation.html")
+        self.assertFalse(response.context["is_activation_successful"])
+        self.assertEqual(response.context["error_message"], "This account is already activated")
+        user1.refresh_from_db()
+        self.assertTrue(user1.is_active)
+
+        user1.is_active = False
+        user1.save()
+
+        # invalid user_id
+        response = self.client.get(reverse("account-activate", args=["abc", token1]))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "users/account_activation.html")
+        self.assertFalse(response.context["is_activation_successful"])
+        self.assertEqual(response.context["error_message"], "Invalid user id")
+        user1.refresh_from_db()
+        user2.refresh_from_db()
+        self.assertFalse(user1.is_active)
+        self.assertFalse(user2.is_active)
+
+        # unencoded user_id
+        response = self.client.get(reverse("account-activate", args=[user1.id, token1]))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "users/account_activation.html")
+        self.assertFalse(response.context["is_activation_successful"])
+        self.assertEqual(response.context["error_message"], "Invalid user id")
+        user1.refresh_from_db()
+        user2.refresh_from_db()
+        self.assertFalse(user1.is_active)
+        self.assertFalse(user2.is_active)
+
+        # non-existent user_id
+        nonexistent_user_id = urlsafe_base64_encode(force_bytes(-1))
+        response = self.client.get(reverse("account-activate", args=[nonexistent_user_id, token1]))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "users/account_activation.html")
+        self.assertFalse(response.context["is_activation_successful"])
+        self.assertEqual(response.context["error_message"], "Invalid user id")
+        user1.refresh_from_db()
+        self.assertFalse(user1.is_active)
+
+        # invalid token
+        response = self.client.get(reverse("account-activate", args=[user1_encoded_id, token2]))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "users/account_activation.html")
+        self.assertFalse(response.context["is_activation_successful"])
+        self.assertEqual(response.context["error_message"], "Invalid token")
+        user1.refresh_from_db()
+        user2.refresh_from_db()
+        self.assertFalse(user1.is_active)
+        self.assertFalse(user1.is_active)
