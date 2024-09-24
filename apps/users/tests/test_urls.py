099179958984370b26200856c29c723b7c2f91d4
nsorokopud <96976346+nsorokopud@users.noreply.github.com>
2024-09-24 16:22:15 +0100

    Add tests for the users app urls

--- apps/users/tests/test_urls.py
+++ apps/users/tests/test_urls.py
@@ -1,11 +1,41 @@
 from allauth.account.views import LogoutView
+from allauth.socialaccount.providers.google import views as google_views
 from django.test import TestCase
 from django.urls import reverse, resolve
 
-from users.views import UserLoginView, UserProfileView, UserRegistrationView
+from users.views import (
+    AccountActivationView,
+    AuthorPageView,
+    AuthorSubscribeView,
+    PasswordSetView,
+    PostUserRegistrationView,
+    UserLoginView,
+    UserProfileView,
+    UserRegistrationView,
+)
 
 
 class TestURLs(TestCase):
+    def test_account_activation_url_is_resolved(self):
+        url = reverse("account-activate", args=["a", "b"])
+        self.assertEqual(resolve(url).func.view_class, AccountActivationView)
+
+    def test_author_page_url_is_resolved(self):
+        url = reverse("author-page", args=["a"])
+        self.assertEqual(resolve(url).func.view_class, AuthorPageView)
+
+    def test_author_subscribe_url_is_resolved(self):
+        url = reverse("author-subscribe", args=["a"])
+        self.assertEqual(resolve(url).func.view_class, AuthorSubscribeView)
+
+    def test_password_set_url_is_resolved(self):
+        url = reverse("password-set")
+        self.assertEqual(resolve(url).func.view_class, PasswordSetView)
+
+    def test_post_user_registration_url_is_resolved(self):
+        url = reverse("post-registration")
+        self.assertEqual(resolve(url).func.view_class, PostUserRegistrationView)
+
     def test_user_registration_url_is_resolved(self):
         url = reverse("registration")
         self.assertEqual(resolve(url).func.view_class, UserRegistrationView)
@@ -13,6 +43,10 @@ class TestURLs(TestCase):
     def test_user_login_url_is_resolved(self):
         url = reverse("login")
         self.assertEqual(resolve(url).func.view_class, UserLoginView)
+
+    def test_google_login_by_token_url_is_resolved(self):
+        url = reverse("google_login_by_token")
+        self.assertEqual(resolve(url).func.view_class, google_views.LoginByTokenView)
 
     def test_user_logout_url_is_resolved(self):
         url = reverse("account_logout")
