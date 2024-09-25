8c77da8a5eee433b043d871f38a6da0b8f5633f9
nsorokopud <96976346+nsorokopud@users.noreply.github.com>
2024-09-25 18:24:04 +0100

    Add USE_HTTPS to settings

--- config/settings.py
+++ config/settings.py
@@ -387,3 +387,8 @@ EMAIL_PORT = os.environ["EMAIL_PORT"]
 EMAIL_HOST_USER = os.environ["EMAIL_HOST_USER"]
 EMAIL_HOST_PASSWORD = os.environ["EMAIL_HOST_PASSWORD"]
 EMAIL_USE_TLS = os.environ["EMAIL_USE_TLS"]
+
+
+# HTTPS
+
+USE_HTTPS = bool(int(os.getenv("USE_HTTPS", False)))
