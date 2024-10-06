9ede173018b35db5d937db5da037a3b85d07d762
nsorokopud <96976346+nsorokopud@users.noreply.github.com>
2024-10-06 18:27:22 +0100

    Update default DB backend in settings

--- config/settings.py
+++ config/settings.py
@@ -110,7 +110,7 @@ ASGI_APPLICATION = "config.asgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "ENGINE": "django.db.backends.postgresql",
         "NAME": os.environ["DB_NAME"],
         "USER": os.environ["DB_USER"],
         "PASSWORD": os.environ["DB_PASSWORD"],
