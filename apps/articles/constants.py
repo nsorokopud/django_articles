1eb1154f6cdd0f99262031f979d4457fac0a6da9
nsorokopud <96976346+nsorokopud@users.noreply.github.com>
2025-03-19 15:32:18 +0000

    Change `os.getenv()` default values from `int` to `str` for numerical constants in `articles` app

--- apps/articles/constants.py
+++ apps/articles/constants.py
@@ -2,7 +2,7 @@ import os
 
 
 # Indicates how many articles will be displayed by paginator on the home page
-ARTICLES_PER_PAGE_COUNT = int(os.getenv("ARTICLES_PER_PAGE_COUNT", 5))
+ARTICLES_PER_PAGE_COUNT = int(os.getenv("ARTICLES_PER_PAGE_COUNT", "5"))
 
 # Indicates how many chars of a long comment will be displayed by __str__ method
-DISPLAYED_COMMENT_LENGTH = int(os.getenv("DISPLAYED_COMMENT_LENGTH", 25))
+DISPLAYED_COMMENT_LENGTH = int(os.getenv("DISPLAYED_COMMENT_LENGTH", "25"))
