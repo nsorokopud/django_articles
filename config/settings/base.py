fd100c1e5b1b6a747892484aa1a5edd55a665c4d
nsorokopud <96976346+nsorokopud@users.noreply.github.com>
2025-04-05 15:04:14 +0100

    Remove redundant `REDIS_HOST` env var loading from Django Channels config

--- config/settings/base.py
+++ config/settings/base.py
@@ -395,7 +395,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [(os.environ["REDIS_HOST"], REDIS_PORT)],
+            "hosts": [(REDIS_HOST, REDIS_PORT)],
         },
     },
 }
