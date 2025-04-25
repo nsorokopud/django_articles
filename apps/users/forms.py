2e5392577e46da39e186841e5804232b41359e3b
nsorokopud <96976346+nsorokopud@users.noreply.github.com>
2025-04-25 20:01:00 +0100

    Add `notification_emails_allowed` field to `ProfileUpdateForm`

--- apps/users/forms.py
+++ apps/users/forms.py
@@ -34,4 +34,5 @@ class UserUpdateForm(forms.ModelForm):
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ["image"]
+        fields = ["image", "notification_emails_allowed"]
+        labels = {"notification_emails_allowed": "Allow notifications via email"}
