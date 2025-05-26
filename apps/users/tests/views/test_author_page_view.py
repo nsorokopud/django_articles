75c16337f4b961710d0e6f180988dcd3ced6b214
nsorokopud <96976346+nsorokopud@users.noreply.github.com>
2025-05-26 22:15:04 +0100

    Fix imports in `users` app's tests

--- apps/users/tests/views/test_author_page_view.py
+++ apps/users/tests/views/test_author_page_view.py
@@ -2,7 +2,7 @@ from django.core.cache import cache
 from django.test import Client, TestCase
 from django.urls import reverse
 
-from apps.users.cache import get_subscribers_count_cache_key
+from users.cache import get_subscribers_count_cache_key
 from users.models import User
 
 
