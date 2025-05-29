ec15dc052da9c53e7d6b4afbfd2c08845c1bccb6
nsorokopud <96976346+nsorokopud@users.noreply.github.com>
2025-05-29 02:51:57 +0100

    Move `PostRegistrationView` tests into a separate file

--- apps/users/tests/views/test_views.py
+++ apps/users/tests/views/test_views.py
@@ -1,21 +1,18 @@
 from django.test import Client, TestCase
 from django.urls import reverse
 
-from users.models import User
 
-
-class TestViews(TestCase):
+class TestPostRegistrationView(TestCase):
     def setUp(self):
         self.client = Client()
 
-        self.test_user = User.objects.create_user(
-            username="test_user", email="test@test.com"
-        )
+        self.url = reverse("post-registration")
 
-    def test_post_registration_view(self):
-        response = self.client.get(reverse("post-registration"))
+    def test_get(self):
+        response = self.client.get(self.url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "users/post_registration.html")
 
-        response = self.client.post(reverse("post-registration"))
+    def test_post(self):
+        response = self.client.post(self.url)
         self.assertEqual(response.status_code, 405)
