f06129e8ecc8a358dd8292e0aba9f7b27f4e30f1
nsorokopud <96976346+nsorokopud@users.noreply.github.com>
2025-07-01 15:32:43 +0100

    Add `MediaSaveError` exception

--- apps/core/exceptions.py
+++ apps/core/exceptions.py
@@ -1,2 +1,6 @@
 class InvalidUpload(Exception):
     """Raised when an uploaded file fails validation."""
+
+
+class MediaSaveError(Exception):
+    """Raised when saving a media file to storage fails."""
