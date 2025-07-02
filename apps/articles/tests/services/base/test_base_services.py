5d954920d2fb1c969f34dd9f53ae8c360b440b20
nsorokopud <96976346+nsorokopud@users.noreply.github.com>
2025-07-02 23:26:43 +0100

    Reorganize tests for article services into a package

---
+++ apps/articles/tests/services/base/test_base_services.py
@@ -0,0 +1,203 @@
+from django.db.utils import IntegrityError
+from django.test import TestCase
+
+from articles.models import Article, ArticleCategory, ArticleComment
+from articles.services import (
+    create_article,
+    generate_unique_article_slug,
+    increment_article_views_counter,
+    toggle_article_like,
+    toggle_comment_like,
+)
+from users.models import User
+
+
+class TestServices(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username="user", email="user@test.com")
+        self.category = ArticleCategory.objects.create(title="cat", slug="cat")
+
+    def test_generate_unique_article_slug(self):
+        self.assertEqual(generate_unique_article_slug("abc"), "abc")
+
+        Article.objects.create(
+            title="abc",
+            slug="abc",
+            author=self.user,
+            preview_text="1",
+            content="1",
+        )
+
+        self.assertEqual(generate_unique_article_slug("abc"), "abc")
+        self.assertEqual(generate_unique_article_slug("abc "), "abc-1")
+
+        Article.objects.create(
+            title="abc-",
+            slug="abc-1",
+            author=self.user,
+            preview_text="1",
+            content="1",
+        )
+        self.assertEqual(generate_unique_article_slug("abc"), "abc")
+        self.assertEqual(generate_unique_article_slug("abc-"), "abc-1")
+        self.assertEqual(generate_unique_article_slug("abc -"), "abc-2")
+
+    def test_create_article(self):
+        a1 = create_article(
+            title="a1",
+            category=self.category,
+            author=self.user,
+            preview_text="text1",
+            content="content1",
+            tags=["tag1", "tag2"],
+        )
+
+        # articles are sorted by '-created_at' by default, so the last one created will
+        # be the first one in queryset
+        last_article = Article.objects.first()
+
+        self.assertEqual(last_article.pk, a1.pk)
+        self.assertEqual(last_article.slug, "a1")
+        self.assertEqual(last_article.author.username, "user")
+
+        expected_tags = ["tag1", "tag2"]
+        actual_tags = [tag.name for tag in last_article.tags.all()]
+        self.assertCountEqual(actual_tags, expected_tags)
+
+    def test_create_article_with_same_title(self):
+        a1 = create_article(
+            title="a1",
+            category=self.category,
+            author=self.user,
+            preview_text="text1",
+            content="content1",
+            tags=["tag1", "tag2"],
+        )
+
+        a2 = create_article(
+            title="a2",
+            category=self.category,
+            author=self.user,
+            preview_text="text1",
+            content="content1",
+        )
+
+        self.assertEqual(a1.slug, "a1")
+        self.assertEqual(a2.slug, "a2")
+
+        with self.assertRaises(IntegrityError):
+            a3 = create_article(
+                title="a1",
+                category=self.category,
+                author=self.user,
+                preview_text="text1",
+                content="content1",
+                tags=["tag1", "tag2"],
+            )
+
+        self.assertEqual(Article.objects.count(), 2)
+        last_article = Article.objects.first()
+        self.assertEqual(last_article, a2)
+
+    def test_create_article_tags_creation(self):
+        a1 = create_article(
+            title="a1",
+            category=self.category,
+            author=self.user,
+            preview_text="text1",
+            content="content1",
+            tags=["tag1", "tag2"],
+        )
+
+        a1_actual_tags = [t.name for t in a1.tags.all()]
+        self.assertCountEqual(a1_actual_tags, ["tag1", "tag2"])
+
+        a2 = create_article(
+            title="a2",
+            category=self.category,
+            author=self.user,
+            preview_text="text1",
+            content="content1",
+        )
+
+        a2_actual_tags = [t.name for t in a2.tags.all()]
+        self.assertCountEqual(a2_actual_tags, [])
+
+    def test_toggle_article_like(self):
+        a = Article.objects.create(
+            title="a1",
+            slug="a1",
+            category=self.category,
+            author=self.user,
+            preview_text="text1",
+            content="content1",
+            is_published=True,
+        )
+
+        user = User(username="user1", email="test@test.com")
+        user.set_password("12345")
+        user.save()
+
+        likes_count = toggle_article_like(a.slug, self.user.id)
+        self.assertEqual(likes_count, 1)
+        likes_count = toggle_article_like(a.slug, self.user.id)
+        self.assertEqual(likes_count, 0)
+
+        likes_count = toggle_article_like(a.slug, self.user.id)
+        self.assertEqual(likes_count, 1)
+        likes_count = toggle_article_like(a.slug, user.id)
+        self.assertEqual(likes_count, 2)
+        likes_count = toggle_article_like(a.slug, self.user.id)
+        self.assertEqual(likes_count, 1)
+        likes_count = toggle_article_like(a.slug, user.id)
+        self.assertEqual(likes_count, 0)
+
+    def test_toggle_comment_like(self):
+        a = Article.objects.create(
+            title="a1",
+            slug="a1",
+            category=self.category,
+            author=self.user,
+            preview_text="text1",
+            content="content1",
+            is_published=True,
+        )
+        comment = ArticleComment.objects.create(
+            article=a, author=self.user, text="text"
+        )
+
+        user = User(username="user1", email="test@test.com")
+        user.set_password("12345")
+        user.save()
+
+        likes_count = toggle_comment_like(comment.id, self.user.id)
+        self.assertEqual(likes_count, 1)
+        likes_count = toggle_comment_like(comment.id, self.user.id)
+        self.assertEqual(likes_count, 0)
+
+        likes_count = toggle_comment_like(comment.id, self.user.id)
+        self.assertEqual(likes_count, 1)
+        likes_count = toggle_comment_like(comment.id, user.id)
+        self.assertEqual(likes_count, 2)
+        likes_count = toggle_comment_like(comment.id, self.user.id)
+        self.assertEqual(likes_count, 1)
+        likes_count = toggle_comment_like(comment.id, user.id)
+        self.assertEqual(likes_count, 0)
+
+    def test_increment_article_views_count(self):
+        a1 = Article.objects.create(
+            title="a1",
+            slug="a1",
+            category=self.category,
+            author=self.user,
+            preview_text="text1",
+            content="content1",
+            is_published=True,
+        )
+        self.assertEqual(a1.views_count, 0)
+        new_views_count = increment_article_views_counter(a1)
+        self.assertEqual(new_views_count, 1)
+        self.assertEqual(a1.views_count, 1)
+        new_views_count = increment_article_views_counter(a1)
+        self.assertEqual(new_views_count, 2)
+        self.assertEqual(a1.views_count, 2)
