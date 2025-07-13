b13399d0702983fa3e32df096a7fec7e0711940b
nsorokopud <96976346+nsorokopud@users.noreply.github.com>
2025-07-13 10:36:54 +0100

    Remove `ArticleCreateForm` and `ArticleUpdateForm`

--- apps/articles/forms.py
+++ apps/articles/forms.py
@@ -5,7 +5,6 @@ from core.exceptions import InvalidUpload
 from core.validators import validate_uploaded_file
 
 from .models import Article, ArticleComment
-from .services import create_article, generate_unique_article_slug
 
 
 class ArticleModelForm(forms.ModelForm):
@@ -38,61 +37,6 @@ class ArticleModelForm(forms.ModelForm):
         if commit:
             instance.save()
             self.save_m2m()
-        return instance
-
-
-class ArticleCreateForm(forms.ModelForm):
-    preview_text = forms.TextInput()
-
-    class Meta:
-        model = Article
-        fields = [
-            "title",
-            "category",
-            "tags",
-            "preview_text",
-            "preview_image",
-            "content",
-        ]
-
-    def __init__(self, *args, **kwargs):
-        self.request = kwargs.pop("request")
-        super().__init__(*args, **kwargs)
-
-    def save(self, **kwargs):
-        super().save(commit=False, **kwargs)
-
-        article = create_article(
-            title=self.cleaned_data["title"],
-            author=self.request.user,
-            preview_text=self.cleaned_data["preview_text"],
-            content=self.cleaned_data["content"],
-            category=self.cleaned_data["category"],
-            tags=self.cleaned_data["tags"],
-            preview_image=self.cleaned_data["preview_image"],
-        )
-        return article
-
-
-class ArticleUpdateForm(forms.ModelForm):
-    preview_text = forms.TextInput()
-
-    class Meta:
-        model = Article
-        fields = [
-            "title",
-            "category",
-            "tags",
-            "preview_text",
-            "preview_image",
-            "content",
-        ]
-
-    def save(self, **kwargs):
-        instance = super().save(commit=False, **kwargs)
-        instance.slug = generate_unique_article_slug(instance.title)
-        instance.save()
-        self.save_m2m()
         return instance
 
 
