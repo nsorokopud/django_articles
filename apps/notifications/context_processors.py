be17d7a001c911ad91afb628150642e256348ee5
nsorokopud <96976346+nsorokopud@users.noreply.github.com>
2025-07-26 11:29:51 +0100

    Modify `include_user_notifications` context processor to handle missing user

--- apps/notifications/context_processors.py
+++ apps/notifications/context_processors.py
@@ -1,15 +1,25 @@
-from .services import (
-    find_notifications_by_user,
-    get_unread_notifications_count_by_user,
-)
+import logging
+from typing import Any
+
+from .services import find_notifications_by_user, get_unread_notifications_count_by_user
 
 
-def include_user_notifications(request):
-    if request.user.is_authenticated:
-        notifications = find_notifications_by_user(request.user.id)
-        notifications_count = get_unread_notifications_count_by_user(request.user)
+logger = logging.getLogger(__name__)
+
+
+def include_user_notifications(request) -> dict[str, Any] | dict:
+    user = getattr(request, "user", None)
+
+    if user is None:
+        logger.warning(
+            "Request (%s) has no user attribute. Path: %s.",
+            type(request),
+            request.get_full_path(),
+        )
+
+    if user and getattr(user, "is_authenticated", False):
         return {
-            "notifications": notifications,
-            "notifications_count": notifications_count,
+            "notifications": find_notifications_by_user(user.id),
+            "notifications_count": get_unread_notifications_count_by_user(user),
         }
     return {}
