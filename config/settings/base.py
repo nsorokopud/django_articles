b88aa4dc197e22f1aea797a058b7f872b1c880f4
nsorokopud <96976346+nsorokopud@users.noreply.github.com>
2026-03-09 19:14:25 +0000

    Replace `include_user_notifications` context processor with `include_notification_count`

--- config/settings/base.py
+++ config/settings/base.py
@@ -121,7 +121,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-                "notifications.context_processors.include_user_notifications",
+                "notifications.context_processors.include_notification_count",
             ],
         },
     },
