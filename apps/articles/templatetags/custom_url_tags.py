256d975406076ea30f43f1759739903e10b40fe4
nsorokopud <96976346+nsorokopud@users.noreply.github.com>
2026-04-30 16:41:03 +0100

    Refactor `relative_url` template tag to use `QueryDict`

--- apps/articles/templatetags/custom_url_tags.py
+++ apps/articles/templatetags/custom_url_tags.py
@@ -4,22 +4,20 @@ from django import template
 register = template.Library()
 
 
-@register.simple_tag
-def relative_url(field_name, value, urlencode=None):
-    """Returns a url with the specified parameters. If 'urlencode' is
-    given, 'field_name' parameter with the specified 'value' will be
-    appended at the end of the url, after all the other parameters. All
-    the duplicate entries of the 'field_name' parameter will be removed.
-    If 'urlencode' is not given, the url will have only one parameter,
-    the one passed as 'field_name'.
-    """
-    url = f"?{field_name}={value}"
-    if urlencode:
-        querystring = urlencode.split("&")
-        filtered_querystring = filter(
-            lambda param: param.split("=")[0] != field_name, querystring
-        )
-        encoded_querystring = "&".join(filtered_querystring)
-        if encoded_querystring != "":
-            url = f"?{encoded_querystring}&{field_name}={value}"
-    return url
+@register.simple_tag(takes_context=True)
+def relative_url(context, field_name, value) -> str:
+    request = context.get("request")
+    if request is None:
+        return ""
+
+    query_dict = request.GET.copy()
+
+    if value in (None, "", []):
+        query_dict.pop(field_name, None)
+    elif isinstance(value, (list, tuple)):
+        query_dict.setlist(field_name, value)
+    else:
+        query_dict[field_name] = value
+
+    encoded = query_dict.urlencode()
+    return f"?{encoded}" if encoded else ""
