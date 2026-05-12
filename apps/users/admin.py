bd91b784162331ec4ede0efe09703a3b4790a309
nsorokopud <96976346+nsorokopud@users.noreply.github.com>
2026-05-12 20:56:07 +0100

    Configure users app admin for custom user model

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,22 +1,62 @@
 from allauth.account.admin import EmailAddressAdmin as AllauthEmailAddressAdmin
 from allauth.account.models import EmailAddress
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from django.utils.html import format_html
 
 from .forms import EmailAddressModelForm
 from .models import Profile, TokenCounter, User
 
 
-admin.site.register((TokenCounter, User))
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    model = User
+
+    readonly_fields = (
+        "latest_article_publish_sequence",
+        "subscriptions_last_seen_publish_sequence",
+        "unread_notifications_count",
+    )
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Article / notification state",
+            {
+                "fields": (
+                    "latest_article_publish_sequence",
+                    "subscriptions_last_seen_publish_sequence",
+                    "unread_notifications_count",
+                )
+            },
+        ),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (("Email", {"fields": ("email",)}),)
+
+    list_display = ("id", "username", "email", "is_active", "is_staff", "is_superuser")
+    list_display_links = ("id", "username")
+    search_fields = ("username", "email")
+    ordering = ("username",)
+
+
+@admin.register(TokenCounter)
+class TokenCounterAdmin(admin.ModelAdmin):
+    list_display = ("user", "token_type", "token_count")
+    search_fields = ("user__username", "user__email", "token_type")
+    list_filter = ("token_type",)
 
 
 @admin.register(Profile)
 class UserProfileAdmin(admin.ModelAdmin):
     list_display = ("user", "get_profile_image")
-    search_fields = ("user__username",)
+    search_fields = ("user__username", "user__email")
     readonly_fields = ("user",)
 
+    @admin.display(description="Profile image")
     def get_profile_image(self, profile):
+        if not profile.image:
+            return "-"
+
         return format_html("<img src='{}' width='35' height='35' />", profile.image.url)
 
 
