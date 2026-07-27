a6a5e26a2d4cd1505e10d47b41364cba10505dcc
nsorokopud <96976346+nsorokopud@users.noreply.github.com>
2026-07-27 12:07:44 +0100

    Rename application branding to Articles

--- apps/users/constants.py
+++ apps/users/constants.py
@@ -1,7 +1,7 @@
-ACTIVATION_EMAIL_SUBJECT = "User account activation at Django Articles"
+ACTIVATION_EMAIL_SUBJECT = "User account activation at Articles"
 ACTIVATION_EMAIL_TEXT_TEMPLATE = "users/emails/activation_email.txt"
 ACTIVATION_EMAIL_HTML_TEMPLATE = "users/emails/activation_email.html"
 
-EMAIL_CHANGE_SUBJECT = "Confirm email change at Django Articles"
+EMAIL_CHANGE_SUBJECT = "Confirm email change at Articles"
 EMAIL_CHANGE_TEXT_TEMPLATE = "users/emails/email_change.txt"
 EMAIL_CHANGE_HTML_TEMPLATE = "users/emails/email_change.html"
